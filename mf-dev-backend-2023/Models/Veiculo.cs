18d74bb97457024033de6bdf9a3bfa81f3549a72
Eduardo Lima <998254@sga.pucminas.br>
2023-10-14 13:14:21 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        public int AnoModelo { get; set; }
+
+    }
+}
