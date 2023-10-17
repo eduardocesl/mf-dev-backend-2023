5836eb6c15a94e5e720cd60f6d89c4a64ea828df
Eduardo Lima <998254@sga.pucminas.br>
2023-10-17 13:00:12 -0300

    CRUD Veículo

--- mf-dev-backend-2023/Models/Veiculo.cs
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2023.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Antes de prosseguir, preencha este campo")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
     }
